ae21694717102402b59aaea4eb29d03cb3cd9536
Paul Nguyen <paulnguyen@paulnguyen-macbookpro4.roam.corp.google.com>
2019-11-08 13:58:50 -0800

    roku, depreciated, and new branch updated

--- dial_reference_roku/dial-reference-master/client/build.sh
+++
@@ -1,4 +0,0 @@
-export TARGET=/usr/local/i686-netflix-linux-gnu-4.3/bin/i686-netflix-linux-gnu-
-export LDFLAGS="-L/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib -Wl,-rpath,/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib -Wl,--allow-shlib-undefined"
-export INCLUDES=-I/usr/local/i686-netflix-linux-gnu-4.3/netflix/include
-make
